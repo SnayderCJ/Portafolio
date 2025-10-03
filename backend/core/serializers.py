043199e82ae38a497aecf09c18a4210949d466af
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2025-10-02 19:21:49 -0500

    Fix image URLs for Cloudinary

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Project, Skill
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
     class Meta:
+        
         model = Project
         fields = '__all__' # O especifica los campos: ['id', 'title', 'description', ...]
 
